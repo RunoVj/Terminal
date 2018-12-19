03cf4741b8fcb6edb73789b5123ac9c071309f5a
Ilya <runodj@gmail.com>
2018-12-19 12:55:49 +0300

    Added simple chart

--- chartswidget.cpp
+++ chartswidget.cpp
@@ -3,12 +3,60 @@
 
 ChartsWidget::ChartsWidget(QWidget *parent) :
     QWidget(parent),
-    ui(new Ui::ChartsWidget)
+    ui(new Ui::ChartsWidget),
+    _chart_ptr(new QChart),
+    _chart_view_ptr(new QChartView),
+    _x_axis_ptr(new QValueAxis),
+    _y_axis_ptr(new QValueAxis),
+    _line_series_ptr(new QLineSeries)
 {
     ui->setupUi(this);
+    _chart_ptr->setAnimationOptions(QChart::AllAnimations);
+    _line_series_ptr->append(3, 4);
+    _line_series_ptr->append(4, 6);
+
+    _chart_ptr->addSeries(_line_series_ptr);
+
+    _x_axis_ptr->setRange(0, 100);
+    _y_axis_ptr->setRange(-10, 10);
+    _x_axis_ptr->setTickCount(11);
+    _x_axis_ptr->setTitleText("Aaaaa, a");
+
+    _chart_view_ptr->chart()->setAxisX(_x_axis_ptr, _line_series_ptr);
+    _chart_view_ptr->chart()->setAxisY(_y_axis_ptr, _line_series_ptr);
+
+    _chart_view_ptr->setRenderHint(QPainter::Antialiasing);
+
+    ui->chartLayout->addWidget(_chart_view_ptr);
 }
 
 ChartsWidget::~ChartsWidget()
 {
     delete ui;
+    delete _chart_ptr;
+    delete _chart_view_ptr;
+    delete _x_axis_ptr;
+    delete _y_axis_ptr;
+    delete _line_series_ptr;
+}
+
+void ChartsWidget::start_timer(bool start)
+{
+
+}
+void ChartsWidget::add_current_data(double value)
+{
+
+}
+void ChartsWidget::add_pwm_data(int8_t value)
+{
+
+}
+void ChartsWidget::add_speed_data(uint16_t value)
+{
+
+}
+void ChartsWidget::add_force_data(uint16_t value)
+{
+
 }
--- chartswidget.h
+++ chartswidget.h
@@ -2,6 +2,9 @@
 #define CHARTSWIDGET_H
 
 #include <QWidget>
+#include <QtCharts>
+#include <QSplineSeries>
+#include <QDebug>
 
 namespace Ui {
 class ChartsWidget;
@@ -17,6 +20,23 @@ public:
 
 private:
     Ui::ChartsWidget *ui;
+    QChart *_chart_ptr;
+    QChartView *_chart_view_ptr;
+    QValueAxis *_x_axis_ptr;
+    QValueAxis *_y_axis_ptr;
+    QLineSeries *_line_series_ptr;
+
+signals:
+
+public slots:
+    void start_timer(bool start);
+    void add_current_data(double value);
+    void add_pwm_data(int8_t value);
+    void add_speed_data(uint16_t value);
+    void add_force_data(uint16_t value);
+
+private slots:
+
 };
 
 #endif // CHARTSWIDGET_H
